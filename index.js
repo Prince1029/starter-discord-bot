b23fc48741ea39d04f2c473d95d8e834c0fb6ab9
Prince1029 <69244392+Prince1029@users.noreply.github.com>
2023-11-23 14:54:49 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,133 +1,111 @@
-const { Client, GatewayIntentBits } = require('discord.js');
-const client = new Client({
-  intents: [
-    GatewayIntentBits.Guilds,
-    GatewayIntentBits.GuildMessages,
-    GatewayIntentBits.MessageContent,
-    GatewayIntentBits.MessageContent,
-    GatewayIntentBits.GuildMembers,
-  ],
-});
+// const { clientId, guildId, token, publicKey } = require('./config.json');
+require('dotenv').config()
+const APPLICATION_ID = process.env.APPLICATION_ID 
+const TOKEN = process.env.TOKEN 
+const PUBLIC_KEY = process.env.PUBLIC_KEY || 'not set'
+const GUILD_ID = process.env.GUILD_ID 
 
-const prefix = '!';
 
-client.once('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
-});
+const axios = require('axios')
+const express = require('express');
+const { InteractionType, InteractionResponseType, verifyKeyMiddleware } = require('discord-interactions');
 
-client.on('messageCreate', async (message) => {
-  if (message.author.bot || !message.content.startsWith(prefix)) return;
 
-  const [command, ...args] = message.content.slice(prefix.length).split(' ');
+const app = express();
+// app.use(bodyParser.json());
 
-  // Verification system
-  if (command === 'verify') {
-    // Implement verification logic here
-    // For example, you might assign a "Verified" role to the user
-    const verifiedRole = message.guild.roles.cache.find((role) => role.name === 'Verified');
-    if (verifiedRole) {
-      message.member.roles.add(verifiedRole);
-      message.reply('You have been verified!');
-    } else {
-      message.reply('Verification role not found. Contact the server administrator.');
-    }
+const discord_api = axios.create({
+  baseURL: 'https://discord.com/api/',
+  timeout: 3000,
+  headers: {
+	"Access-Control-Allow-Origin": "*",
+	"Access-Control-Allow-Methods": "GET, POST, PUT, DELETE",
+	"Access-Control-Allow-Headers": "Authorization",
+	"Authorization": `Bot ${TOKEN}`
   }
+});
 
-  // Chat logs channel creation command
-  if (command === 'createchatlogschannel' && message.member.permissions.has('MANAGE_CHANNELS')) {
-    const guild = message.guild;
-    const channelName = 'chat-logs';
 
-    // Check if the channel already exists
-    const existingChannel = guild.channels.cache.find((channel) => channel.name === channelName);
-    if (existingChannel) {
-      message.reply('Chat logs channel already exists.');
-    } else {
-      // Create the channel
-      try {
-        const createdChannel = await guild.channels.create(channelName, {
-          type: 'text',
-          topic: 'Chat logs will be recorded here.',
-        });
-        message.reply(`Chat logs channel created: ${createdChannel}`);
-      } catch (error) {
-        console.error('Error creating chat logs channel:', error);
-        message.reply('An error occurred while creating the chat logs channel.');
+
+
+app.post('/interactions', verifyKeyMiddleware(PUBLIC_KEY), async (req, res) => {
+  const interaction = req.body;
+
+  if (interaction.type === InteractionType.APPLICATION_COMMAND) {
+    console.log(interaction.data.name)
+    if(interaction.data.name == 'yo'){
+      return res.send({
+        type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
+        data: {
+          content: `Yo ${interaction.member.user.username}!`,
+        },
+      });
+    }
+
+    if(interaction.data.name == 'dm'){
+      // https://discord.com/developers/docs/resources/user#create-dm
+      let c = (await discord_api.post(`/users/@me/channels`,{
+        recipient_id: interaction.member.user.id
+      })).data
+      try{
+        // https://discord.com/developers/docs/resources/channel#create-message
+        let res = await discord_api.post(`/channels/${c.id}/messages`,{
+          content:'Yo! I got your slash command. I am not able to respond to DMs just slash commands.',
+        })
+        console.log(res.data)
+      }catch(e){
+        console.log(e)
       }
+
+      return res.send({
+        // https://discord.com/developers/docs/interactions/receiving-and-responding#responding-to-an-interaction
+        type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
+        data:{
+          content:'👍'
+        }
+      });
     }
   }
-
-  // Moderation commands
-  if (command === 'purge' && message.member.permissions.has('MANAGE_MESSAGES')) {
-    const deleteCount = parseInt(args[0], 10) || 100;
-    message.channel.bulkDelete(deleteCount);
-  } else if (command === 'kick' && message.member.permissions.has('KICK_MEMBERS')) {
-    const member = message.mentions.members.first();
-    if (member) {
-      member.kick();
-      message.channel.send(`${member.user.tag} has been kicked.`);
-    }
-  } else if (command === 'ban' && message.member.permissions.has('BAN_MEMBERS')) {
-    const member = message.mentions.members.first();
-    if (member) {
-      member.ban();
-      message.channel.send(`${member.user.tag} has been banned.`);
-    }
-  } else if (command === 'mute' && message.member.permissions.has('MUTE_MEMBERS')) {
-    // Implement mute logic here
-    message.channel.send('Muting functionality coming soon!');
-  }
-
-  // Music commands (using a placeholder for simplicity)
-  if (command === 'play') {
-    // Implement music play command
-    message.channel.send('Playing music coming soon!');
-  }
-
-  // Invite Tracker
-  if (command === 'invitetracker') {
-    // Implement invite tracker logic
-    message.channel.send('Invite tracker functionality coming soon!');
-  }
-
-  // Welcoming new users
-  if (command === 'welcome') {
-    if (message.member.permissions.has('MANAGE_CHANNELS')) {
-      const welcomeChannel = message.guild.channels.cache.find((channel) => channel.name === 'welcome');
-      welcomeChannel.send(`Welcome to the server, ${message.author.username}!`);
-    }
-  }
-
-  // Custom command for moderators
-  if (command === 'customcommand' && message.member.permissions.has('KICK_MEMBERS')) {
-    // Your custom command logic here
-    message.channel.send('This is a custom command for moderators!');
-  }
-
-  // Announcements
-  if (command === 'announce' && message.member.permissions.has('ADMINISTRATOR')) {
-    const announcementChannel = message.guild.channels.cache.find((channel) => channel.name === 'announcements');
-    announcementChannel.send(args.join(' '));
-  }
-
-  // Role management
-  if (command === 'giverole' && message.member.permissions.has('MANAGE_ROLES')) {
-    const roleName = args.join(' ');
-    const role = message.guild.roles.cache.find((r) => r.name === roleName);
-    if (role) {
-      message.member.roles.add(role);
-      message.channel.send(`You now have the ${roleName} role!`);
-    } else {
-      message.channel.send(`Role "${roleName}" not found.`);
-    }
-  }
-
-  // In-chat dashboard (using a placeholder for simplicity)
-  if (command === 'dashboard' && message.member.permissions.has('ADMINISTRATOR')) {
-    // Implement in-chat dashboard logic
-    message.channel.send('Dashboard functionality coming soon!');
-  }
-
-  // Add more commands as needed...
 
 });
+
+
+
+app.get('/register_commands', async (req,res) =>{
+  let slash_commands = [
+    {
+      "name": "yo",
+      "description": "replies with Yo!",
+      "options": []
+    },
+    {
+      "name": "dm",
+      "description": "sends user a DM",
+      "options": []
+    }
+  ]
+  try
+  {
+    // api docs - https://discord.com/developers/docs/interactions/application-commands#create-global-application-command
+    let discord_response = await discord_api.put(
+      `/applications/${APPLICATION_ID}/guilds/${GUILD_ID}/commands`,
+      slash_commands
+    )
+    console.log(discord_response.data)
+    return res.send('commands have been registered')
+  }catch(e){
+    console.error(e.code)
+    console.error(e.response?.data)
+    return res.send(`${e.code} error from discord`)
+  }
+})
+
+
+app.get('/', async (req,res) =>{
+  return res.send('Follow documentation ')
+})
+
+
+app.listen(8999, () => {
+
+})
