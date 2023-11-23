b6141496f0049456f3ac5cd50ffb76975b46a10e
Prince1029 <69244392+Prince1029@users.noreply.github.com>
2023-11-23 21:22:55 +0800

    Update discord.js

--- discord.js
+++ discord.js
@@ -1,5 +1,9 @@
 const { Client, GatewayIntentBits } = require('discord.js');
-const ytdl = require('ytdl-core');
+const axios = require('axios');
+const express = require('express');
+const { InteractionType, InteractionResponseType, verifyKeyMiddleware } = require('discord-interactions');
+require('dotenv').config();
+
 const client = new Client({
   intents: [
     GatewayIntentBits.Guilds,
@@ -52,28 +56,15 @@ client.on('messageCreate', async (message) => {
   if (command === 'purge' && message.member.permissions.has('MANAGE_MESSAGES')) {
     const deleteCount = parseInt(args[0], 10) || 100;
     message.channel.bulkDelete(deleteCount);
-  } else if (command === 'kick' && message.member.permissions.has('KICK_MEMBERS')) {
-    const member = message.mentions.members.first();
-    if (member) {
-      member.kick();
-      message.channel.send(`${member.user.tag} has been kicked.`);
-    }
-  } else if (command === 'ban' && message.member.permissions.has('BAN_MEMBERS')) {
-    const member = message.mentions.members.first();
-    if (member) {
-      member.ban();
-      message.channel.send(`${member.user.tag} has been banned.`);
-    }
-  } else if (command === 'mute' && message.member.permissions.has('MUTE_MEMBERS')) {
-    // Implement mute logic here
-    message.channel.send('Muting functionality coming soon!');
   }
+  // Add other moderation commands (kick, ban, mute) as needed...
 
   // Music commands (using a placeholder for simplicity)
   if (command === 'play') {
     // Implement music play command
     message.channel.send('Playing music coming soon!');
   }
+  // Add other music-related commands as needed...
 
   // Invite Tracker
   if (command === 'invitetracker') {
@@ -119,113 +110,92 @@ client.on('messageCreate', async (message) => {
     message.channel.send('Dashboard functionality coming soon!');
   }
 
-  // Music recommendation command
-  if (command === 'musicrec') {
-    const genre = args.join(' ').toLowerCase();
-    const genreMap = {
-      'pop': ['pop music', 'pop songs'],
-      'rock': ['rock music', 'classic rock', 'alternative rock'],
-      'hiphop': ['hip hop music', 'rap music'],
-      'classical': ['classical music', 'symphony'],
-      'edm': ['electronic dance music', 'house music'],
-      'jazz': ['jazz music', 'smooth jazz'],
-      'country': ['country music', 'bluegrass'],
-      'metal': ['metal music', 'heavy metal'],
-      'rnb': ['rhythm and blues music', 'soul music'],
-      'indie': ['indie music', 'indie rock'],
-      'reggae': ['reggae music', 'dancehall'],
-      'blues': ['blues music', 'electric blues'],
-      'punk': ['punk music', 'pop punk'],
-      'folk': ['folk music', 'folk rock'],
-      'latin': ['latin music', 'salsa'],
-      // Add more genres and their corresponding search queries including sub-genres
-    };
-
-    if (genre in genreMap) {
-      const subGenres = genreMap[genre];
-      const randomSubGenre = getRandomElement(subGenres);
-      const searchQuery = randomSubGenre || genreMap[genre][0]; // Use the first genre if no sub-genres
-      const videoUrl = await getYouTubeUrl(searchQuery);
-
-      if (videoUrl) {
-        message.reply(`Here's a ${genre} recommendation (${randomSubGenre || 'main genre'}): ${videoUrl}`);
-      } else {
-        message.reply(`Sorry, I couldn't find a recommendation for ${genre}.`);
-      }
-    } else {
-      const supportedGenres = Object.keys(genreMap).join(', ');
-      message.reply(`Invalid genre. Supported genres: ${supportedGenres}`);
-    }
-  }
-
-  // ... (other commands)
-
-});
-
-client.on('messageCreate', async (message) => {
-  if (message.author.bot || !message.content.startsWith(prefix)) return;
-
-  const [command, ...args] = message.content.slice(prefix.length).split(' ');
-
-  // Verification system
-  if (command === 'verify') {
-    // Implement verification logic here
-    // For example, you might assign a "Verified" role to the user
-    const verifiedRole = message.guild.roles.cache.find((role) => role.name === 'Verified');
-    if (verifiedRole) {
-      message.member.roles.add(verifiedRole);
-      message.reply('You have been verified!');
-    } else {
-      message.reply('Verification role not found. Contact the server administrator.');
-    }
-  }
-
-  // Chat logs channel creation command
-  if (command === 'createchatlogschannel' && message.member.permissions.has('MANAGE_CHANNELS')) {
-    const guild = message.guild;
-    const channelName = 'chat-logs';
-
-    // Check if the channel already exists
-    const existingChannel = guild.channels.cache.find((channel) => channel.name === channelName);
-    if (existingChannel) {
-      message.reply('Chat logs channel already exists.');
-    } else {
-      // Create the channel
-      try {
-        const createdChannel = await guild.channels.create(channelName, {
-          type: 'text',
-          topic: 'Chat logs will be recorded here.',
-        });
-        message.reply(`Chat logs channel created: ${createdChannel}`);
-      } catch (error) {
-        console.error('Error creating chat logs channel:', error);
-        message.reply('An error occurred while creating the chat logs channel.');
-      }
-    }
-  }
-
-  // ... (other commands)
+  // Add more commands as needed...
 
 });
 
 // ... (remaining code)
 
-client.login(process.env.DISCORD_TOKEN);
+// Slash command handling
+const app = express();
 
-async function getYouTubeUrl(searchQuery) {
-  try {
-    const searchResults = await ytdl.getBasicInfo(`ytsearch:${searchQuery}`);
-    if (searchResults && searchResults.videoDetails) {
-      return `https://www.youtube.com/watch?v=${searchResults.videoDetails.videoId}`;
+app.use(express.json());
+
+app.post('/interactions', verifyKeyMiddleware(process.env.PUBLIC_KEY), async (req, res) => {
+  const interaction = req.body;
+
+  if (interaction.type === InteractionType.APPLICATION_COMMAND) {
+    console.log(interaction.data.name);
+
+    if (interaction.data.name === 'yo') {
+      return res.send({
+        type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
+        data: {
+          content: `Yo ${interaction.member.user.username}!`,
+        },
+      });
     }
-    return null;
-  } catch (error) {
-    console.error('Error fetching YouTube URL:', error);
-    return null;
-  }
-}
 
-function getRandomElement(array) {
-  return array[Math.floor(Math.random() * array.length)];
-}
+    if (interaction.data.name === 'dm') {
+      let c = (await axios.post(`https://discord.com/api/users/@me/channels`, {
+        recipient_id: interaction.member.user.id,
+      })).data;
+
+      try {
+        let res = await axios.post(`https://discord.com/api/channels/${c.id}/messages`, {
+          content: 'Yo! I got your slash command. I am not able to respond to DMs just slash commands.',
+        });
+        console.log(res.data);
+      } catch (e) {
+        console.log(e);
+      }
+
+      return res.send({
+        type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
+        data: {
+          content: '👍',
+        },
+      });
+    }
+  }
+});
+
+app.get('/register_commands', async (req, res) => {
+  let slashCommands = [
+    {
+      name: 'yo',
+      description: 'replies with Yo!',
+      options: [],
+    },
+    {
+      name: 'dm',
+      description: 'sends user a DM',
+      options: [],
+    },
+  ];
+
+  try {
+    let discordResponse = await axios.put(
+      `https://discord.com/api/applications/${process.env.APPLICATION_ID}/guilds/${process.env.GUILD_ID}/commands`,
+      slashCommands
+    );
+    console.log(discordResponse.data);
+    return res.send('Commands have been registered');
+  } catch (e) {
+    console.error(e.code);
+    console.error(e.response?.data);
+    return res.send(`${e.code} error from Discord`);
+  }
+});
+
+app.get('/', async (req, res) => {
+  return res.send('Follow documentation ');
+});
+
+app.listen(8999, () => {
+  console.log('Express app listening on port 8999');
+});
+
+// Login to Discord with your bot token
+client.login(process.env.DISCORD_TOKEN);
 
