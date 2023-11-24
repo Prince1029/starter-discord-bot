e486587f0ba80c922a2c7fc28a57b269c0c6ce18
Prince1029 <69244392+Prince1029@users.noreply.github.com>
2023-11-24 11:17:48 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
-const { Client, GatewayIntentBits } = require('discord.js');
-const ytdl = require('ytdl-core');
+const { Client, GatewayIntentBits, Collection } = require('discord.js');
 const axios = require('axios');
 const express = require('express');
 const { InteractionType, InteractionResponseType, verifyKeyMiddleware } = require('discord-interactions');
+const helmet = require('helmet');
 require('dotenv').config();
-
 
 const client = new Client({
   intents: [
@@ -21,15 +20,118 @@ client.once('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
+// Collection to store custom commands
+const customCommands = new Collection();
+
 client.on('messageCreate', async (message) => {
-  // ... (your existing message handling code)
+  if (message.author.bot) return;
+
+  // Check for custom commands
+  const customCommand = customCommands.get(message.content.toLowerCase());
+  if (customCommand) {
+    message.channel.send(customCommand);
+    return;
+  }
+
+  if (!message.content.startsWith(prefix)) return;
+
+  const [command, ...args] = message.content.slice(prefix.length).split(' ');
+
+  // Help and Commands commands
+  if (command === 'help') {
+    // ... (existing help command)
+  }
+
+  // Custom command for moderators
+  if (command === 'customcommand' && message.member.permissions.has('KICK_MEMBERS')) {
+    // ... (existing custom command)
+  }
+
+  // Announcements
+  if (command === 'announce' && message.member.permissions.has('ADMINISTRATOR')) {
+    // ... (existing announce command)
+  }
+
+  // Role management
+  if (command === 'giverole' && message.member.permissions.has('MANAGE_ROLES')) {
+    // ... (existing giverole command)
+  }
+
+  // In-chat dashboard
+  if (command === 'dashboard' && message.member.permissions.has('ADMINISTRATOR')) {
+    // ... (existing dashboard command)
+  }
+
+  // Random Meme Command
+  if (command === 'randommeme') {
+    // ... (existing randommeme command)
+  }
+
+  // Emoji Menu
+  if (command === 'emojimenu') {
+    // ... (existing emojimenu command)
+  }
+
+  // Meme Maker/Generator
+  if (command === 'makememe') {
+    // ... (existing makememe command)
+  }
+
+  // Music Recommendations
+  if (command === 'musicrec') {
+    const genre = args.join(' ');
+
+    if (genre) {
+      try {
+        // Get Spotify access token
+        const authResponse = await axios.post(
+          'https://accounts.spotify.com/api/token',
+          new URLSearchParams({
+            grant_type: 'client_credentials',
+          }),
+          {
+            headers: {
+              'Content-Type': 'application/x-www-form-urlencoded',
+              Authorization: `Basic ${Buffer.from(
+                `${process.env.SPOTIFY_CLIENT_ID}:${process.env.SPOTIFY_CLIENT_SECRET}`
+              ).toString('base64')}`,
+            },
+          }
+        );
+
+        const accessToken = authResponse.data.access_token;
+
+        // Search for a track based on the specified genre
+        const searchResponse = await axios.get(
+          `https://api.spotify.com/v1/search?q=${genre}&type=track&limit=1`,
+          {
+            headers: {
+              Authorization: `Bearer ${accessToken}`,
+            },
+          }
+        );
+
+        const track = searchResponse.data.tracks.items[0];
+        const trackUrl = track.external_urls.spotify;
+
+        message.channel.send(`Check out this awesome ${genre} song recommendation: ${trackUrl}`);
+      } catch (error) {
+        console.error('Error fetching music recommendation:', error);
+        message.channel.send('An error occurred while fetching the music recommendation.');
+      }
+    } else {
+      message.channel.send('Please specify a genre for the music recommendation.');
+    }
+  }
+
+  // Add more commands as needed...
 });
 
-// ... (your other event handlers)
-
+// Express web server
 const app = express();
 
 app.use(express.json());
+app.use(helmet());
 
 app.post('/interactions', verifyKeyMiddleware(process.env.PUBLIC_KEY), async (req, res) => {
   const interaction = req.body;
@@ -37,73 +139,24 @@ app.post('/interactions', verifyKeyMiddleware(process.env.PUBLIC_KEY), async (re
   if (interaction.type === InteractionType.APPLICATION_COMMAND) {
     console.log(interaction.data.name);
 
-    if (interaction.data.name == 'yo') {
-      return res.send({
-        type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
-        data: {
-          content: `Yo ${interaction.member.user.username}!`,
-        },
-      });
+    if (interaction.data.name === 'yo') {
+      // ... (existing yo slash command)
     }
 
-    if (interaction.data.name == 'dm') {
-      let c = (await axios.post(`https://discord.com/api/users/@me/channels`, {
-        recipient_id: interaction.member.user.id,
-      })).data;
-
-      try {
-        let res = await axios.post(`https://discord.com/api/channels/${c.id}/messages`, {
-          content: 'Yo! I got your slash command. I am not able to respond to DMs, just slash commands.',
-        });
-        console.log(res.data);
-      } catch (e) {
-        console.log(e);
-      }
-
-      return res.send({
-        type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
-        data: {
-          content: '👍',
-        },
-      });
+    if (interaction.data.name === 'dm') {
+      // ... (existing dm slash command)
     }
   }
 });
 
 app.get('/register_commands', async (req, res) => {
-  let slashCommands = [
-    {
-      name: 'yo',
-      description: 'replies with Yo!',
-      options: [],
-    },
-    {
-      name: 'dm',
-      description: 'sends user a DM',
-      options: [],
-    },
-  ];
-
-  try {
-    let discordResponse = await axios.put(
-      `https://discord.com/api/applications/${process.env.APPLICATION_ID}/guilds/${process.env.GUILD_ID}/commands`,
-      slashCommands
-    );
-    console.log(discordResponse.data);
-    return res.send('Commands have been registered');
-  } catch (e) {
-    console.error(e.code);
-    console.error(e.response?.data);
-    return res.send(`${e.code} error from Discord`);
-  }
+  // ... (existing register_commands logic)
 });
 
-app.get('/', async (req, res) => {
-  return res.send('Follow documentation ');
+const PORT = process.env.PORT || 8999;
+app.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`);
 });
 
-app.listen(8999, () => {
-  console.log('Express app listening on port 8999');
-});
-
+// Login to Discord with your bot token
 client.login(process.env.DISCORD_TOKEN);
